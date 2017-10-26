13f73d7e973a8f71d0b28ef280eb189f1ed1e838
shiota yoshiki <shiotyan317@gmail.com>
2017-10-27 02:21:12 +0900

    no message

--- app/assets/javascripts/channels/room.coffee
+++ app/assets/javascripts/channels/room.coffee
@@ -6,7 +6,7 @@ App.room = App.cable.subscriptions.create "RoomChannel",
     # Called when the subscription has been terminated by the server
 
   received: (data) ->
-    alert data['message']
+     $('#messages').append data['message']
       # Called when there's incoming data on the websocket for this channel
 
   speak: (message)->
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008093234) do
+ActiveRecord::Schema.define(version: 20171026162822) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20171008093234) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.boolean "isDoctor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
