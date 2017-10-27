799cd565f6a7e12f8c58a291d5991ac41d1ad108
shiota yoshiki <shiotyan317@gmail.com>
2017-10-27 16:23:12 +0900

    no message

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  # config = ActiveRecord :: Base.configurations [Rails.env]
+  # config ['reaping_frequency'] = ENV ['DB_REAP_FREQ'] || 10
+  # config ['pool'] = ENV ['DB_POOL'] || 20
+  # ActiveRecord :: Base.establish_connection(config)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
 	get 'signup'  => 'users#new'
- resources :users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
