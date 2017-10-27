c79836e8faf4b5c6589a78f97f24582a79707094
shiota yoshiki <shiotyan317@gmail.com>
2017-10-27 14:13:41 +0900

    redis serring

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module YepChat
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+		config.after_initialize do
+		  PasswordAgingJob.perform_later
+		  SetOffCallJob.perform_later
+		end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
