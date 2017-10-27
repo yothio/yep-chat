377888d0896b7eb01d442de00e1f038b67feb944
shiota yoshiki <shiotyan317@gmail.com>
2017-10-27 20:33:28 +0900

    no message

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,6 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
   # for session store.
-  # config.session_store_servers = 'redis://localhost:6379/0'
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,5 @@
-# uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
-# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 require 'redis'
 
 uri = URI.parse(ENV["REDIS_URL"])
 REDIS = Redis.new(host: uri.host, port: uri.port)
-# $redis = Redis.new(url: ENV["REDIS_URL"])
